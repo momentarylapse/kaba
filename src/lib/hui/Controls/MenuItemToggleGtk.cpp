f2d8592fe18100982145f0a97757c8374fe74f19
Michael Ankele <michi@lupina.de>
2020-05-20 22:28:57 +0200

    hui update

--- src/lib/hui/Controls/MenuItemToggleGtk.cpp
+++ src/lib/hui/Controls/MenuItemToggleGtk.cpp
@@ -12,22 +12,20 @@
 namespace hui
 {
 
-gboolean OnGtkMenuClick(GtkWidget *widget, gpointer data);
+gboolean on_get_menu_click(GtkWidget *widget, gpointer data);
 
 MenuItemToggle::MenuItemToggle(const string &title, const string &id) :
 	Control(MENU_ITEM_TOGGLE, id)
 {
 	widget = gtk_check_menu_item_new_with_label(get_lang_sys(id, title, false));
-	g_signal_connect(G_OBJECT(widget), "activate", G_CALLBACK(OnGtkMenuClick), this);
+	g_signal_connect(G_OBJECT(widget), "activate", G_CALLBACK(on_get_menu_click), this);
 }
 
-void MenuItemToggle::__check(bool checked)
-{
+void MenuItemToggle::__check(bool checked) {
 	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(widget), checked);
 }
 
-bool MenuItemToggle::is_checked()
-{
+bool MenuItemToggle::is_checked() {
 	return gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(widget));
 }
 
