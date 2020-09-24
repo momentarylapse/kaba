e88df1c3d8b873b2203a79af372db6444d287ce2
Michael Ankele <michi@lupina.de>
2020-09-24 19:43:36 +0200

    minor fixes (mem alloc range etc)

--- Tests/Regression/regression.sh
+++ Tests/Regression/regression.sh
@@ -1,26 +1,38 @@
 kaba="kaba"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 if [ -n "$1" ]
 then
 	kaba="$1"
 fi
 
 
-
+ERRORS=0
 
 for f in *.reg
 do
 	k=${f%reg}kaba
-	printf "%-32s" "$k"
+	printf "%-36s" "$k"
 	"$kaba" "$k" > out
 	x=`diff "$f" out`
 	if [ -n "$x" ]
 	then
-		echo "Fehler"
-		echo "$x"
+		echo -e "${RED}ERROR${NC}"
+		echo -e "${RED}$x${NC}"
+		ERRORS=`expr $ERRORS + 1`
 	else
-		echo "ok"
+		echo -e "${GREEN}ok${NC}"
 	fi
 done
 
 rm -f out
+
+if [ $ERRORS = 0 ]
+then
+	echo -e "${GREEN}all tests passed${NC}"
+else
+	echo -e "${RED}$ERRORS tests failed${NC}"
+fi
