cd56c7f9d1817f25559f5342f32deca9f3cf4526
Michael Ankele <michi@lupina.de>
2020-12-25 02:19:50 +0100

    lib update

--- src/lib/nix/nix_view.h
+++ src/lib/nix/nix_view.h
@@ -41,6 +41,8 @@ public:
 	~FrameBuffer();
 	void __init__(const Array<Texture*> &attachments);
 	void __delete__();
+	void update(const Array<Texture*> &attachments);
+	void update_x(const Array<Texture*> &attachments, int cube_face);
 	Array<Texture*> color_attachments;
 	DepthBuffer *depth_buffer;
 	unsigned int frame_buffer;
