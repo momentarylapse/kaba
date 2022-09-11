6d1cdc36ae0576e39dd9da0b66bc94e075363512
Michael Ankele <michi@lupina.de>
2022-09-12 00:38:23 +0200

    fix: sorted() parsing

--- src/lib/kaba/parser/Parser.cpp
+++ src/lib/kaba/parser/Parser.cpp
@@ -1328,7 +1328,8 @@ shared<Node> Parser::parse_abstract_statement_sorted(Block *block) {
 	int token0 = Exp.consume_token(); // "sorted"
 
 	auto node = add_node_statement(StatementID::SORTED, token0, TypeUnknown);
-	if (Exp.end_of_line() or Exp.peek_next() != "(") {
+
+	if (Exp.cur != "(") {
 		node->set_num_params(0);
 		return node;
 	}
