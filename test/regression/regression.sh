fcdd2b65469064f0f0157638814acb03267ac0a9
Michael Ankele <michi@lupina.de>
2023-02-20 11:21:41 +0100

    [tests] split into test suites (folders)

--- test/regression/regression.sh
+++ test/regression/regression.sh
@@ -1,38 +1,93 @@
-kaba="kaba"
+KABA="kaba"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if [ -n "$1" ]
-then
-	kaba="$1"
-fi
+VERBOSE=0
+SUITES_FILTER=""
 
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-v|--verbose)
+			VERBOSE=1
+			shift
+			;;
+		-s|--suites)
+			SUITES_FILTER=$2
+			shift
+			shift
+			;;
+		-k|--kaba)
+			KABA="$2"
+			shift
+			shift
+			;;
+		*)
+			echo "unknown option: $1"
+			shift
+			;;
+	esac
+done
 
-ERRORS=0
+TOTAL_FAILED=0
+TOTAL_TESTS=0
 
-for f in *.reg
+for D in *
 do
-	k=${f%reg}kaba
-	printf "%-36s" "$k"
-	"$kaba" "$k" > out
-	x=`diff --strip-trailing-cr "$f" out`
-	if [ -n "$x" ]
+	if [ ! -d $D ]
 	then
-		echo -e "${RED}ERROR${NC}"
-		echo -e "${RED}$x${NC}"
-		ERRORS=`expr $ERRORS + 1`
-	else
-		echo -e "${GREEN}ok${NC}"
+		continue
 	fi
+	if [[ -n "$SUITES_FILTER" && $D != "$SUITES_FILTER" ]]
+	then
+		continue
+	fi
+	
+#	echo $D
+	printf "%-20s" "$D"
+
+	FAILED=0
+	TESTS=0
+	PASSED=0
+
+	for f in $D/*.reg
+	do
+		k=${f%reg}kaba
+		"$KABA" "$k" > out
+		x=`diff --strip-trailing-cr "$f" out`
+		if [ -n "$x" ]
+		then
+			if [ $VERBOSE -gt 0 ]
+			then
+				echo $f
+				echo -e "${RED}ERROR${NC}"
+				echo -e "${RED}$x${NC}"
+			fi
+			FAILED=`expr $FAILED + 1`
+		else
+#			echo -e "${GREEN}ok${NC}"
+			PASSED=`expr $PASSED + 1`
+		fi
+		
+		TESTS=`expr $TESTS + 1`
+	done
+	
+	if [ $FAILED = 0 ]
+	then
+		echo -e "    ${GREEN}passed: $PASSED / $TESTS${NC}"
+	else
+		echo -e "    ${RED}passed: $PASSED / $TESTS  ($FAILED failed)${NC}"
+	fi
+	TOTAL_TESTS=`expr $TOTAL_TESTS + $TESTS`
+	TOTAL_FAILED=`expr $TOTAL_FAILED + $FAILED`
 done
 
 rm -f out
 
-if [ $ERRORS = 0 ]
+if [ $TOTAL_FAILED = 0 ]
 then
 	echo -e "${GREEN}all tests passed${NC}"
 else
-	echo -e "${RED}$ERRORS tests failed${NC}"
+	echo -e "${RED}$TOTAL_FAILED tests failed${NC}"
 fi
